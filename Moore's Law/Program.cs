5fdaa3852905e2fd6095b43ad1214d782ba72a82
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2020-10-08 15:30:14 +0300

    Moore's Law Act.

---
+++ Moore's Law/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moore_s_Law
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
