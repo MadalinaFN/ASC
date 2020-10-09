384ce311d2a1a66a6cbac61b324cfdbf667861a1
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2020-10-09 13:00:47 +0300

    Conversie

---
+++ Conversie/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Converteste 14 din baza 10 in baza 2
+
+            Int32 num = 14;
+            Console.WriteLine(Convert.ToString(num, 2));
+
+        }
+    }
+}
--- Moore's Law/Program.cs
+++ Moore's Law/Program.cs
@@ -10,6 +10,12 @@ namespace Moore_s_Law
     {
         static void Main(string[] args)
         {
+            //Legea lui Moore spune ca puterea de calcul se dubleaza la fiecare 18 luni, iar pretul ramane acelasi.
+            //Dupa cat timp vom avea o putere de calcul de 100 de ori mai mare fata de cat avem la un moment dat, la acelasi pret?
+            //Dupa cat timp vom avea o putere de calcul de n ori mai mare, la acelasi pret?
+
+
+            
         }
     }
 }
