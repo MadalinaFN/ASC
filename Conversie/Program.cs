c96d8438fdfdd0471a9f3c360204de6637787156
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2020-10-30 17:14:22 +0200

    Conversie

--- Conversie/Program.cs
+++ Conversie/Program.cs
@@ -17,39 +17,75 @@ namespace Conversie
             //Scrieti un program C# care converteste un numar din baza b1 in baza b2.
             //Numarul pe care il convertim va fi un numar reprezentat in virgula fixa.
 
-            Console.WriteLine("Conversie dintr-o baza intr-o alta baza tinta");
+            //***** Am vrut sa scriu acest program astfel incat sa converteasca orice numar din orice baza in orice baza dar, *****
+            //***** fiind incepatoare, nu prea m-am descurcat si am reusit doar ceea ce este in continuare: *****
 
-            string base2 = "10101";
-            string base8 = "10101";
-            string base10 = "10101";
-            string base16 = "10101";
+            int b1, b2, num;
 
-            Console.WriteLine("Conversie din baza 2 in alta baza");
-            Console.WriteLine("Convert.ToInt32(base2, 2) = " + Convert.ToInt32(base2, 2));
-            Console.WriteLine("Convert.ToInt32(base2, 8) = " + Convert.ToInt32(base2, 8));
-            Console.WriteLine("Convert.ToInt32(base2, 10) = " + Convert.ToInt32(base2, 10));
-            Console.WriteLine("Convert.ToInt32(base2, 16) = " + Convert.ToInt32(base2, 16));
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("Introduceti numarul 2 pentru a face o conversie din baza 2 intr-o alta baza");
+            Console.WriteLine("Introduceti numarul 4 pentru a face o conversie din baza 4 in baza 10");
+            Console.WriteLine("Introduceti numarul 10 pentru a face o conversie din baza 10 intr-o alta baza");
+            Console.WriteLine("Introduceti numarul 16 pentru a face o conversie din baza 16 in baza 10");
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            b1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Conversie din baza 8 in alta baza");
-            Console.WriteLine("Convert.ToInt32(base8, 2) = " + Convert.ToInt32(base8, 2));
-            Console.WriteLine("Convert.ToInt32(base8, 8) = " + Convert.ToInt32(base8, 8));
-            Console.WriteLine("Convert.ToInt32(base8, 10) = " + Convert.ToInt32(base8, 10));
-            Console.WriteLine("Convert.ToInt32(base8, 16) = " + Convert.ToInt32(base8, 16));
+            switch (b1)
+            {
+                case 2: //Converteste orice numar din baza doi in bazele 2,8,10,16
+                    Console.WriteLine("Introduceti un numar in baza 2");
+                    num = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Conversie din baza 10 in alta baza");
-            Console.WriteLine("Convert.ToInt32(base10, 2) = " + Convert.ToInt32(base10, 2));
-            Console.WriteLine("Convert.ToInt32(base10, 8) = " + Convert.ToInt32(base10, 8));
-            Console.WriteLine("Convert.ToInt32(base10, 10) = " + Convert.ToInt32(base10, 10));
-            Console.WriteLine("Convert.ToInt32(base10, 16) = " + Convert.ToInt32(base10, 16));
+                    Console.WriteLine("Introduceti baza in care vreti sa convertiti numarul (2,8,10,16)");
+                    b2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Conversie din baza 16 in alta baza");
-            Console.WriteLine("Convert.ToInt32(base16, 2) = " + Convert.ToInt32(base16, 2));
-            Console.WriteLine("Convert.ToInt32(base16, 8) = " + Convert.ToInt32(base16, 8));
-            Console.WriteLine("Convert.ToInt32(base16, 10) = " + Convert.ToInt32(base16, 10));
-            Console.WriteLine("Convert.ToInt32(base16, 16) = " + Convert.ToInt32(base16, 16));
+                    Console.WriteLine("Numarul convertit este " + Convert.ToString(num, b2));
+                    break;
 
+                case 4: //Converteste orice numar de maxim 3 cifre din baza patru in baza 10
+                    Console.WriteLine("Introduceti un numar de maxim 3 cifre in baza 4");
+                    num = int.Parse(Console.ReadLine());
 
+                    int m, s, z, u;
+                    double numconvertit;
 
+                    s = num / 100 % 10;
+                    z = num / 10 % 10;
+                    u = num % 10;
+
+                    numconvertit = s * (Math.Pow(4, 2)) + z * 4 + u;
+
+                    Console.WriteLine("Numarul convertit este {0}", numconvertit);
+                    break;
+
+                case 10: //Converteste orice numar din baza 10 in bazele 2,8,10,16
+                    Console.WriteLine("Introduceti un numar in baza 10");
+                    Int32 n = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Introduceti baza in care vreti sa convertiti numarul (2,8,10,16)");
+                    b2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Numarul convertit este " + Convert.ToString(n, b2));
+                    break;
+
+                case 16: //Converteste orice numare de maxim 4 cifre din baza 16 in baza 10
+                    Console.WriteLine("Introduceti un numar de maxim 4 cifre in baza 16");
+                    num = int.Parse(Console.ReadLine());
+
+                    m = num / 1000 % 10;
+                    s = num / 100 % 10;
+                    z = num / 10 % 10;
+                    u = num % 10;
+
+                    numconvertit = m * (Math.Pow(16, 3)) + s * (Math.Pow(16, 2)) + z * 16 + u;
+
+                    Console.WriteLine("Numarul convertit este {0}", numconvertit);
+                    break;
+
+                default:
+                    Console.WriteLine("Numarul introdus nu e corect");
+                    break;
+            }
         }
     }
 }
